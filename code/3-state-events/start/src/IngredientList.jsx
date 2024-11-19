24f04f9152a8b5c39ee0d68f1d8794cbb23970bf
Robert Martinez <rmartinez@pacifico-meetings.com>
2024-11-19 22:59:12 +0100

    finish module state and events

--- code/3-state-events/start/src/IngredientList.jsx
+++ code/3-state-events/start/src/IngredientList.jsx
@@ -1,22 +1,22 @@
 import './IngredientList.css'
 import React from 'react';
 
-function IngredientList(props) {
+function IngredientList (props) {
     const ingredientListItems = props.ingredients.map((ingredient, index) => {
         return (
-            <li key={index} 
-                className={ ingredient.prepared ? 'prepared' : '' }
+            <li key={index}
+                className={ingredient.prepared ? 'prepared' : ''}
                 // TODO: Add onClick event
-                
+                onClick={() => props.onClick(index)}
             >
-                { ingredient.name }
+                {ingredient.name}
             </li>
         );
     });
 
     return (
         <ul>
-            { ingredientListItems }
+            {ingredientListItems}
         </ul>
     );
 }
